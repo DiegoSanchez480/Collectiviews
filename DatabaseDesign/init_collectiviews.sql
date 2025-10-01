dcad059aa6cc57039e36d26c421d617d40b6fd3a
DiegoSanchez480 <112047409+DiegoSanchez480@users.noreply.github.com>
2025-10-01 18:07:09 +0000

    Syntax update; Database creation statement added

--- DatabaseDesign/init_collectiviews.sql
+++ DatabaseDesign/init_collectiviews.sql
@@ -1,31 +1,37 @@
+CREATE DATABASE Collectiviews_DB;
+
+CREATE OR REPLACE ROLE Fantastic4_DBA;
+
+GRANT ALL PRIVILEGES ON Collectiviews_DB TO Fantastic4_DBA;
+
 CREATE TABLE `movie` (
   `movieID` int PRIMARY KEY NOT NULL,
   `title` VARCHAR(100) NOT NULL,
   `director` VARCHAR(50),
   `genre` VARCHAR(30),
-  `releaseDate` date NOT NULL
+  `releaseDate` DATE NOT NULL
 );
 
 CREATE TABLE `user` (
   `username` VARCHAR(30) PRIMARY KEY NOT NULL,
   `name` VARCHAR(50),
   `password_hash` VARCHAR(30) NOT NULL,
-  `birthYear` year NOT NULL
+  `birthYear` DATE NOT NULL
 );
 
 CREATE TABLE `review` (
   `username` VARCHAR(30) NOT NULL,
-  `movieID` int NOT NULL,
-  `rating` tinyint NOT NULL,
+  `movieID` INT NOT NULL,
+  `rating` TINYINT NOT NULL,
   `writtenReview` VARCHAR(2000),
-  `reviewDate` datetime NOT NULL,
+  `reviewDate` DATETIME NOT NULL,
   PRIMARY KEY (`username`, `movieID`)
 );
 
 CREATE TABLE `watchedMovie` (
   `username` VARCHAR(30) NOT NULL,
-  `movieID` int NOT NULL,
-  `watchedDate` date NOT NULL,
+  `movieID` INT NOT NULL,
+  `watchedDate` DATE NOT NULL,
   PRIMARY KEY (`username`, `movieID`)
 );
 
